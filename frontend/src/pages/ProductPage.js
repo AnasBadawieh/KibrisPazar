e2ab882c75cc93b725ae064ebfa7d8c9dab2ff36
Anas <a.badawiah@gmail.com>
2024-11-22 05:31:54 +0200

    ProductPage Multiple IMG Preview

--- frontend/src/pages/ProductPage.js
+++ frontend/src/pages/ProductPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import { Link, useParams } from 'react-router-dom';
@@ -7,15 +7,22 @@ import { listProductDetails } from '../redux/actions/productActions';
 const ProductPage = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
+
   const productDetails = useSelector((state) => state.productDetails);
   const { loading, error, product } = productDetails;
+
+  const [selectedImageIndex, setSelectedImageIndex] = useState(0);
 
   useEffect(() => {
     dispatch(listProductDetails(id));
   }, [dispatch, id]);
 
+  const handleImageClick = (index) => {
+    setSelectedImageIndex(index);
+  };
+
   const imageUrl = product && product.images && product.images.length > 0 
-    ? `${process.env.REACT_APP_API_BASE_URL}${product.images[0]}` 
+    ? `${process.env.REACT_APP_API_BASE_URL}${product.images[selectedImageIndex]}` 
     : '';
 
   return (
@@ -31,6 +38,20 @@ const ProductPage = () => {
         <Row>
           <Col md={6}>
             <Image src={imageUrl} alt={product.name} fluid />
+            <Row className="mt-3">
+              {product.images && product.images.map((img, index) => (
+                <Col key={index} xs={3} md={2}>
+                  <Image
+                    src={`${process.env.REACT_APP_API_BASE_URL}${img}`}
+                    alt={product.name}
+                    fluid
+                    onClick={() => handleImageClick(index)}
+                    className="img-thumbnail"
+                    style={{ cursor: 'pointer' }}
+                  />
+                </Col>
+              ))}
+            </Row>
           </Col>
           <Col md={3}>
             <ListGroup variant="flush">
